e576964347729c53bbd91d3021db936c3ed49863
Firdaus Alamanda <145105296+Pumpkins20@users.noreply.github.com>
2026-05-31 19:07:43 +0700

    feat : fecth real API
    mengubah fecth mode dari mock ke http client serta menerapkan adapter pattern

--- backend/internal/fetcher/http_fetcher.go
+++ backend/internal/fetcher/http_fetcher.go
@@ -2,7 +2,10 @@ package fetcher
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	"backend/internal/models"
@@ -12,16 +15,87 @@ type HttpFetcher struct {
 	client *http.Client
 }
 
+type DummyJsonResponse struct {
+	Products []struct {
+		Stock int `json:"stock"`
+	} `json:"products"`
+}
+
+type FakeStoreResponse struct {
+	Rating struct {
+		Count int `json:"count"`
+	} `json:"rating"`
+}
+
 func (f *HttpFetcher) Fetch(ctx context.Context, supplier models.Supplier) models.FetchResult {
-	msg := "HTTP fetcher not implemented yet in v1.0"
-	return models.FetchResult{
+	start := time.Now()
+
+	result := models.FetchResult{
 		SupplierID:   supplier.ID,
 		SupplierName: supplier.Name,
 		Description:  supplier.Description,
 		Stock:        0,
 		Status:       "ERROR",
 		LatencyMs:    0,
-		FetchedAt:    time.Now(),
-		ErrorMessage: &msg,
+		FetchedAt:    start,
 	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, supplier.EndpointURL, nil)
+	if err != nil {
+		errMsg := fmt.Sprintf("Failed to create request: %v", err)
+		result.ErrorMessage = &errMsg
+		result.LatencyMs = time.Since(start).Microseconds()
+		return result
+	}
+
+	resp, err := f.client.Do(req)
+	if err != nil {
+		errMsg := fmt.Sprintf("Network error or Timeout: %v", err)
+		result.ErrorMessage = &errMsg
+		result.LatencyMs = time.Since(start).Microseconds()
+		return result
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		errMsg := fmt.Sprintf("Supplier return HTTP %d", resp.StatusCode)
+		result.ErrorMessage = &errMsg
+		result.LatencyMs = time.Since(start).Microseconds()
+		return result
+	}
+
+	// Adapter Pattern Proccess parsing JSON
+	var totalStock int
+
+	if strings.Contains(supplier.EndpointURL, "dummyjson.com") {
+		var data DummyJsonResponse
+		if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+			errMsg := "Failed to parsed DummyJSON format"
+			result.ErrorMessage = &errMsg
+			return result
+		}
+		for _, p := range data.Products {
+			totalStock += p.Stock
+		}
+	} else if strings.Contains(supplier.EndpointURL, "fakestoreapi.com") {
+		var data []FakeStoreResponse
+		if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+			errMsg := "Failed to parsed FakeStore format"
+			result.ErrorMessage = &errMsg
+			return result
+		}
+		for _, p := range data {
+			totalStock += p.Rating.Count
+		}
+	} else {
+		errMsg := "Unsupported supplier URL format"
+		result.ErrorMessage = &errMsg
+		return result
+	}
+
+	result.Stock = totalStock
+	result.Status = "SUCCESS"
+	result.LatencyMs = time.Since(start).Microseconds()
+
+	return result
 }
